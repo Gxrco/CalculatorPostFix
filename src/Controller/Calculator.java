5855bff81e7aaff9c721c046a98e9997d4ab0a44
XavierLopez25 <lop22716@uvg.edu.gt>
2023-01-29 12:33:33 -0600

    Función getValue inicializada, su propósito es realizar las operaciones correspondientes de la expresión postfix y dar su resultado.

--- src/Controller/Calculator.java
+++ src/Controller/Calculator.java
@@ -1,5 +1,58 @@
 package Controller;
 
+import Model.PostfixCalculator;
+import Model.Stack;
+
 public class Calculator {
+
+    private Stack<Integer> memory;
+    private String postFix;
+
+    PostfixCalculator pc = new PostfixCalculator();
+
+
+
+    public Calculator(){
+        memory = new Stack<>();
+        postFix = "";
+    }
+
+    public int getValue(String postFix, int x){
+        for (int i =0; i<postFix.length(); i++){
+            char ch = postFix.charAt(i);
+            if('0' <= ch && ch<='9'){
+                memory.push(ch-'0');
+            }else{
+                switch(ch){
+                    case 'x':
+                        memory.push(x);
+                        break;
+
+                    case '+':
+                        int b = memory.pull();
+                        int a = memory.pull();
+                        pc.suma(a,b);
+
+                    case '-':
+                        int d = memory.pull();
+                        int c = memory.pull();
+                        pc.resta(c,d);
+
+                    case '*':
+                        int f = memory.pull();
+                        int e = memory.pull();
+                        pc.resta(e,f);
+
+                    case '/':
+                        int h = memory.pull();
+                        int g = memory.pull();
+                        pc.resta(g,h);
+                }
+            }
+        }
+
+        return memory.pull();
+
+    }
 
 }
--- src/Model/PostfixCalculator.java
+++ src/Model/PostfixCalculator.java
@@ -1,33 +1,37 @@
 package Model;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PostfixCalculator implements IPostfixCalculator{
 
 
     @Override
     public boolean isOneItem(IStack operandos) {
-        return false;
+        return operandos.count() == 1;
+
     }
 
     @Override
     public int suma(int a, int b) {
-        return 0;
+        return a+b;
     }
 
     @Override
     public int resta(int a, int b) {
-        return 0;
+        return a-b;
     }
 
     @Override
     public int multiplicacion(int a, int b) {
-        return 0;
+        return a*b;
     }
 
     @Override
     public int division(int a, int b) {
-        return 0;
+        return a/b;
     }
 
     @Override
@@ -37,6 +41,8 @@ public class PostfixCalculator implements IPostfixCalculator{
 
     @Override
     public ArrayList<String> getItems(String _expresion) {
-        return null;
+        String[] splitInput = _expresion.split("");
+        List<String> list = Arrays.asList(splitInput);
+        return (ArrayList<String>) list;
     }
 }
